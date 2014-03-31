b0d4a7ea389ff341b75af85d0417abe986209260
Denise Fernandez <dcbfernandez@gmail.com>
2014-04-01 00:31:06 +1100

    Added Twitter oAuth

--- app/controllers/Twitter.java
+++ app/controllers/Twitter.java
@@ -1,18 +1,19 @@
 package controllers;
 
-
-import akka.japi.Option;
-import com.google.common.base.Strings;
-import play.api.mvc.Result;
-import play.libs.F;
+import play.libs.F.Option;
 import play.libs.OAuth;
+import play.libs.OAuth.ConsumerKey;
+import play.libs.OAuth.RequestToken;
+import play.libs.OAuth.ServiceInfo;
 import play.mvc.Controller;
+import play.mvc.Result;
+
+import com.google.common.base.Strings;
 
 public class Twitter extends Controller {
+    static final ConsumerKey KEY = new ConsumerKey("...", "...");
 
-    static final OAuth.ConsumerKey KEY = new ConsumerKey("...", "...");
-
-    private static final OAuth.ServiceInfo SERVICE_INFO = new OAuth.ServiceInfo("https://api.twitter.com/oauth/request_token",
+    private static final ServiceInfo SERVICE_INFO = new ServiceInfo("https://api.twitter.com/oauth/request_token",
             "https://api.twitter.com/oauth/access_token",
             "https://api.twitter.com/oauth/authorize",
             KEY);
@@ -22,29 +23,29 @@ public class Twitter extends Controller {
     public static Result auth() {
         String verifier = request().getQueryString("oauth_verifier");
         if (Strings.isNullOrEmpty(verifier)) {
-            String url = controllers.routes.Twitter.auth().absoluteURL(request());
-            OAuth.RequestToken requestToken = TWITTER.retrieveRequestToken(url);
+//            String url = controllers.routes.Twitter.auth().absoluteURL(request());
+            String url = "";
+
+            RequestToken requestToken = TWITTER.retrieveRequestToken(url);
             saveSessionTokenPair(requestToken);
             return redirect(TWITTER.redirectUrl(requestToken.token));
         } else {
-            OAuth.RequestToken requestToken = getSessionTokenPair().get();
-            OAuth.RequestToken accessToken = TWITTER.retrieveAccessToken(requestToken, verifier);
+            RequestToken requestToken = getSessionTokenPair().get();
+            RequestToken accessToken = TWITTER.retrieveAccessToken(requestToken, verifier);
             saveSessionTokenPair(accessToken);
             return redirect(controllers.routes.Application.index());
         }
     }
 
-    private static void saveSessionTokenPair(OAuth.RequestToken requestToken) {
+    private static void saveSessionTokenPair(RequestToken requestToken) {
         session("token", requestToken.token);
         session("secret", requestToken.secret);
     }
 
-    static Option<OAuth.RequestToken> getSessionTokenPair() {
+    static Option<RequestToken> getSessionTokenPair() {
         if (session().containsKey("token")) {
-            return Option.Some(new OAuth.RequestToken(session("token"), session("secret")));
+            return Option.Some(new RequestToken(session("token"), session("secret")));
         }
         return Option.None();
     }
-
-
 }
